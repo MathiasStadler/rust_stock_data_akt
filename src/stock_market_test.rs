f5cccd00cbb05284f926f8b06e1f1e57e888c5f8
Mathias <email@example.com>
2024-08-17 17:10:59 +0000

    save

--- src/stock_market_test.rs
+++ src/stock_market_test.rs
@@ -14,6 +14,7 @@ use crate::stock_market::StockData;
 use rand::Rng;
 use rust_decimal::Decimal;
 use rust_decimal::prelude::FromPrimitive;
+use rust_decimal_macros::dec;
 
 
 #[cfg(test)]
@@ -122,4 +123,354 @@ fn it_creates_a_new_stock_information_with_data_series() {
         Some(stock_data_series_with_change) => assert_eq!(stock_data_series_with_change.len(), 7),
         None => println!("No stock data series found."),
     }
+}
+
+#[test]
+fn it_creates_a_new_stock_information_with_data_series_and_finds_specific_stock_data() {
+    let stock_data_series = generate_stock_data_series(Some(7));
+    let stock_information = StockInformation::new(
+        "BenCorpo".to_string(),
+        "BNCRP".to_string(),
+        stock_data_series,
+    );
+    let stock_date = generate_utc_date_from_date_string("10-06-2022 00:00");
+    let stock_data = stock_information.get_change_of_stock_data_with_given_datetime(stock_date);
+
+    println!("stock_data {:?}", stock_data);
+
+    println!("stock_information {:?}", stock_information);
+
+    match stock_data {
+        Some(_) => {
+            assert!(true)
+        }
+        None => println!("No stock data found."),
+    }
+}
+
+#[test]
+fn it_creates_a_new_stock_information_with_data_series_and_does_not_find_a_specific_stock_data() {
+    let stock_data_series = generate_stock_data_series(Some(7));
+    let stock_information = StockInformation::new(
+        "BenCorpo".to_string(),
+        "BNCRP".to_string(),
+        stock_data_series,
+    );
+    let stock_date = generate_utc_date_from_date_string("10-25-2022 00:00");
+    let stock_data = stock_information.get_change_of_stock_data_with_given_datetime(stock_date);
+
+    println!("stock_data {:?}", stock_data);
+
+    println!("stock_information {:?}", stock_information);
+
+    match stock_data {
+        Some(_) => {
+            println!("Stock data found.")
+        }
+        None => assert!(true),
+    }
+}
+
+#[test]
+fn it_creates_a_new_stock_information_with_data_series_and_gets_moving_averages() {
+    let stock_data_series = vec![
+        StockData::new(
+            generate_utc_date_from_date_string("10-10-2022 00:00"),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(121.00).unwrap().round_dp(2),
+        ),
+        StockData::new(
+            generate_utc_date_from_date_string("10-10-2022 00:00"),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(122.00).unwrap().round_dp(2),
+        ),
+        StockData::new(
+            generate_utc_date_from_date_string("10-10-2022 00:00"),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(120.00).unwrap().round_dp(2),
+        ),
+        StockData::new(
+            generate_utc_date_from_date_string("10-10-2022 00:00"),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(119.00).unwrap().round_dp(2),
+        ),
+        StockData::new(
+            generate_utc_date_from_date_string("10-10-2022 00:00"),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(124.00).unwrap().round_dp(2),
+        ),
+        StockData::new(
+            generate_utc_date_from_date_string("10-10-2022 00:00"),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(128.00).unwrap().round_dp(2),
+        ),
+        StockData::new(
+            generate_utc_date_from_date_string("10-10-2022 00:00"),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(126.00).unwrap().round_dp(2),
+        ),
+    ];
+    let stock_information = StockInformation::new(
+        "BenCorpo".to_string(),
+        "BNCRP".to_string(),
+        stock_data_series,
+    );
+
+    let moving_averages = stock_information.get_moving_averages(4);
+
+    println!("moving_averages {:?}", moving_averages);
+    match moving_averages {
+        Some(moving_averages) => {
+            assert_eq!(moving_averages.len(), 4);
+            assert_eq!(
+                moving_averages[0],
+                Decimal::from_f64(120.50).unwrap().round_dp(2)
+            );
+            assert_eq!(
+                moving_averages[1],
+                Decimal::from_f64(121.25).unwrap().round_dp(2)
+            );
+            assert_eq!(
+                moving_averages[2],
+                Decimal::from_f64(122.75).unwrap().round_dp(2)
+            );
+            assert_eq!(
+                moving_averages[3],
+                Decimal::from_f64(124.25).unwrap().round_dp(2)
+            );
+        }
+        None => println!("No moving averages found!"),
+    }
+}
+
+#[test]
+fn it_creates_a_new_stock_information_with_data_series_and_gets_moving_averages_two() {
+    let stock_data_series = vec![
+        StockData::new(
+            generate_utc_date_from_date_string("10-10-2022 00:00"),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(35.02).unwrap().round_dp(2),
+        ),
+        StockData::new(
+            generate_utc_date_from_date_string("10-10-2022 00:00"),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(35.01).unwrap().round_dp(2),
+        ),
+        StockData::new(
+            generate_utc_date_from_date_string("10-10-2022 00:00"),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(34.65).unwrap().round_dp(2),
+        ),
+        StockData::new(
+            generate_utc_date_from_date_string("10-10-2022 00:00"),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(36.09).unwrap().round_dp(2),
+        ),
+        StockData::new(
+            generate_utc_date_from_date_string("10-10-2022 00:00"),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(35.32).unwrap().round_dp(2),
+        ),
+        StockData::new(
+            generate_utc_date_from_date_string("10-10-2022 00:00"),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(35.50).unwrap().round_dp(2),
+        ),
+        StockData::new(
+            generate_utc_date_from_date_string("10-10-2022 00:00"),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(35.03).unwrap().round_dp(2),
+        ),
+        StockData::new(
+            generate_utc_date_from_date_string("10-10-2022 00:00"),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(35.79).unwrap().round_dp(2),
+        ),
+        StockData::new(
+            generate_utc_date_from_date_string("10-10-2022 00:00"),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(37.07).unwrap().round_dp(2),
+        ),
+        StockData::new(
+            generate_utc_date_from_date_string("10-10-2022 00:00"),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(10.00).unwrap().round_dp(2),
+            Decimal::from_f64(36.05).unwrap().round_dp(2),
+        ),
+    ];
+    let stock_information = StockInformation::new(
+        "BenCorpo".to_string(),
+        "BNCRP".to_string(),
+        stock_data_series,
+    );
+
+    let moving_averages = stock_information.get_moving_averages(5);
+    match moving_averages {
+        Some(moving_averages) => {
+            assert_eq!(moving_averages.len(), 6);
+            assert_eq!(
+                moving_averages[0],
+                Decimal::from_f64(35.22).unwrap().round_dp(2)
+            );
+            assert_eq!(
+                moving_averages[1],
+                Decimal::from_f64(35.32).unwrap().round_dp(2)
+            );
+            assert_eq!(
+                moving_averages[2],
+                Decimal::from_f64(35.32).unwrap().round_dp(2)
+            );
+            assert_eq!(
+                moving_averages[3],
+                Decimal::from_f64(35.55).unwrap().round_dp(2)
+            );
+            assert_eq!(
+                moving_averages[4],
+                Decimal::from_f64(35.75).unwrap().round_dp(2)
+            );
+            assert_eq!(
+                moving_averages[5],
+                Decimal::from_f64(35.90).unwrap().round_dp(2)
+            );
+        }
+        None => println!("No moving averages found!"),
+    }
+}
+
+#[test]
+fn it_creates_a_new_stock_information_with_data_series_and_gets_no_moving_averages() {
+    // Lacks moving averages data due to ma days input is greater than the stock data series available.
+    let stock_data_series = generate_stock_data_series(Some(4));
+    let stock_information = StockInformation::new(
+        "BenCorpo".to_string(),
+        "BNCRP".to_string(),
+        stock_data_series,
+    );
+
+    let moving_averages = stock_information.get_moving_averages(5);
+
+    println!("moving_averages {:?}", moving_averages);
+    match moving_averages {
+        Some(_) => {
+            println!("Moving averages found!")
+        }
+        None => assert!(true),
+    }
+}
+
+#[test]
+fn it_creates_a_new_stock_information_with_data_series_and_show_chart_with_moving_average() {
+    let stock_data_series = generate_stock_data_series(Some(14));
+    let stock_information = StockInformation::new(
+        "BenCorpo".to_string(),
+        "BNCRP".to_string(),
+        stock_data_series,
+    );
+
+    let ma_days = vec![7, 2, 0];
+    let chart = stock_information.show_chart(ma_days, None, None, None);
+
+    match chart {
+        Ok(_) => {
+            assert!(true)
+        }
+        Err(err) => println!("Error in saving chart {:?}", err),
+    }
+}
+
+
+#[test]
+fn it_creates_a_new_stock_information_with_data_series_and_does_not_show_chart() {
+    let stock_data_series = generate_stock_data_series(Some(0));
+    let stock_information = StockInformation::new(
+        "BenCorpo".to_string(),
+        "BNCRP".to_string(),
+        stock_data_series,
+    );
+
+    let ma_days: Vec<u16> = vec![10];
+    let chart = stock_information.show_chart(ma_days, None, None, None);
+
+    println!("chart {:?}", chart);
+    match chart {
+        Ok(_) => {
+            println!("There's a chart");
+        }
+        Err(err) => {
+            println!("Error in saving chart {:?}", err);
+            assert!(true);
+        }
+    }
+}
+
+#[test]
+fn it_gets_post_split_data() {
+    let post_split_data =
+        StockInformation::get_post_split_data(dec!(5), dec!(4), dec!(942), dec!(56));
+
+    assert_eq!(post_split_data.0, dec!(1177));
+    assert_eq!(post_split_data.1, dec!(44.80));
+    assert_eq!(post_split_data.2, dec!(22.40));
+
+    let post_split_data =
+        StockInformation::get_post_split_data(dec!(1), dec!(20), dec!(580_000_000), dec!(0.64));
+    println!("{:?} KAMOTE", post_split_data);
+
+    assert_eq!(post_split_data.0, dec!(29_000_000));
+    assert_eq!(post_split_data.1, dec!(12.80));
+    assert_eq!(post_split_data.2, dec!(0));
+}
+
+#[test]
+fn it_gets_dividend_income() {
+    let dividend_income = StockInformation::get_dividend_income(dec!(2000), dec!(0.51), dec!(4));
+
+    assert_eq!(dividend_income, dec!(4080));
+}
+
+#[test]
+fn it_gets_yield_rate() {
+    let yield_rate = StockInformation::get_yield_rate(dec!(1.4), dec!(57.4));
+
+    assert_eq!(yield_rate, dec!(2.4));
+}
+
+#[test]
+fn it_gets_interest_income() {
+    let interest_income = StockInformation::get_interest_income(dec!(5.7), dec!(1000));
+
+    assert_eq!(interest_income, dec!(57));
 }
