885e44ce7e223a6064fe4a08306aa884ad5575ec
Mathias <email@example.com>
2024-08-17 16:05:50 +0000

    compile ok

--- src/stock_market.rs
+++ src/stock_market.rs
@@ -11,12 +11,10 @@
 //         Color, IntoFont, RGBColor, BLUE, GREEN, RED, WHITE,
 //     },
 // };
-use rust_decimal::Decimal;
-use chrono::Utc;
 use chrono::DateTime;
-// use rust_decimal_macros::dec;
-
-
+use chrono::Utc;
+use rust_decimal::Decimal;
+use rust_decimal_macros::dec;
 
 // net_change and net_change_percent will be computed.
 #[derive(Debug)]
@@ -49,3 +47,65 @@ impl StockData {
         }
     }
 }
+
+#[derive(Debug)]
+pub struct StockInformation {
+    company_name: String,
+    symbol: String,
+    stock_data_series: Vec<StockData>,
+}
+
+impl StockInformation {
+    pub fn new(company_name: String, symbol: String, stock_data_series: Vec<StockData>) -> Self {
+        Self {
+            company_name,
+            symbol,
+            stock_data_series,
+        }
+    }//finished new
+
+    pub fn get_change_of_stock_data_series(&self) -> Option<Vec<StockData>> {
+
+        if self.stock_data_series.len() == 0 {
+            return None;
+        }
+
+        let mut stock_data_series_with_change: Vec<StockData> = vec![];
+        for (index, stock_data) in self.stock_data_series.iter().enumerate() {
+            if index == 0 {
+                stock_data_series_with_change.push(StockData {
+                    date: stock_data.date,
+                    high: stock_data.high,
+                    low: stock_data.low,
+                    open: stock_data.open,
+                    close: stock_data.close,
+                    net_change: Some(dec!(0.0)),
+                    net_change_percent: Some(dec!(0.0)),
+                });
+            } else {
+                let previous_day_close = &self.stock_data_series[index - 1].close;
+                let current_day_close = stock_data.close;
+
+                let net_change = current_day_close - previous_day_close;
+                let net_change_percent = (net_change / previous_day_close) * dec!(100.0);
+
+                stock_data_series_with_change.push(StockData {
+                    date: stock_data.date,
+                    high: stock_data.high,
+                    low: stock_data.low,
+                    open: stock_data.open,
+                    close: stock_data.close,
+                    net_change: Some(net_change.round_dp(2)),
+                    net_change_percent: Some(net_change_percent.round_dp(2)),
+                });
+            }
+        }
+
+
+
+        Some(stock_data_series_with_change)
+    }
+
+    
+    
+}// finished impl
--- src/stock_market_test.rs
+++ src/stock_market_test.rs
@@ -18,6 +18,7 @@ use rust_decimal::prelude::FromPrimitive;
 
 #[cfg(test)]
 // use super::*;
+use crate::stock_market::StockInformation;
 
 fn generate_utc_date_from_date_string(date_string: &str) -> DateTime<Utc> {
     let day_one = NaiveDateTime::parse_from_str(date_string, "%m-%d-%Y %H:%M").unwrap();
@@ -82,4 +83,43 @@ fn generate_stock_data(date_string: &str) -> StockData {
         open,
         close,
     )
+}
+
+fn generate_stock_data_series(limit: Option<u8>) -> Vec<StockData> {
+    let mut stock_data_series: Vec<StockData> = vec![];
+    for number in 0..limit.unwrap_or(7) {
+        let number_plus = number + 1;
+
+        let stock_date = match number_plus {
+            number_plus if number_plus >= 10 => format!("10-{number_plus}-2022 00:00"),
+            _ => format!("10-0{number_plus}-2022 00:00"),
+        };
+
+        let stock_data = generate_stock_data(&stock_date);
+        stock_data_series.push(stock_data);
+    }
+    stock_data_series
+}
+
+#[test]
+fn it_creates_a_new_stock_information_with_data_series() {
+    let stock_data_series = generate_stock_data_series(Some(7));
+    let stock_information = StockInformation::new(
+        "BenCorpo".to_string(),
+        "BNCRP".to_string(),
+        stock_data_series,
+    );
+    let stock_data_series_with_change = stock_information.get_change_of_stock_data_series();
+
+    println!(
+        "stock_data_series_with_change {:?}",
+        stock_data_series_with_change
+    );
+
+    println!("stock_information {:?}", stock_information);
+
+    match stock_data_series_with_change {
+        Some(stock_data_series_with_change) => assert_eq!(stock_data_series_with_change.len(), 7),
+        None => println!("No stock data series found."),
+    }
 }
